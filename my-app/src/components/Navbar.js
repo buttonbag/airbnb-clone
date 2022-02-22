3cb4e2bac96b3b8619ee2a6ab817cc707e78d624
louie camacho dxa <louiecamacho@gmail.com>
2022-02-22 10:39:17 -0600

    images and styles placed

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+
+function Navbar() {
+	return (
+		<nav>
+			<img src={logo} alt="airbnb" className="nav--logo" />
+		</nav>
+	);
+}
+
+export default Navbar;
