4e34085fef51e74797be6ea3c1cfc8f7245c8f88
louie camacho dxa <louiecamacho@gmail.com>
2022-02-24 10:13:15 -0600

    data imported

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,20 +3,29 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import expData from './data';
 
 function App() {
+	const experiences = expData.map((exp) => {
+		// console.log(exp.coverImg);
+		return (
+			<Card
+				key={exp.id}
+				title={exp.title}
+				price={exp.price}
+				img={exp.coverImg}
+				level={exp.stats.rating}
+				total={exp.stats.reviewCount}
+				location={exp.location}
+				openSpots={exp.openSpots}
+			/>
+		);
+	});
 	return (
 		<div className="main">
 			<Navbar />
 			<Hero />
-			<Card
-				img="katie-zaferes.png"
-				rating={5.0}
-				total={6}
-				location="USA"
-				name="Life lessons with Katie Zaferes"
-				price={136}
-			/>
+			<section className="cards-container">{experiences}</section>
 		</div>
 	);
 }
